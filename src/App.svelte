e78633fade0808a11467a4ccc0b04104875e0e8c
Tom Claes <tom.claes@cegeka.com>
2020-09-12 23:04:40 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Invert from './components/Invert.svelte';
+	import { bgColor } from './store.js'
+	export let name;
+
+	let color = 'green';
+
+	const unsubscribe = bgColor.subscribe(val => {
+		color = val;
+	})
+
+</script>
+
+<div class="container" style='--main-bg-color: {color}'>
+	<main>
+		<h1>Hello {name}!</h1>
+		<Invert />
+	</main>
+</div>
+
+
+<style>
+
+	:global(body) {
+		margin: 0;
+	}
+
+	.container {
+		background: var(--main-bg-color);
+		margin: 0;
+		padding: 0;
+		height: 100vh;
+		width: 100vw;
+	}
+	
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Invert.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { bgColor } from '../store';
+  let darkMode = false;
+  let color = [
+    'white', 'black'
+  ]
+
+	function switchMode(){
+    bgColor.set(darkMode ? color[0] : color[1]);
+    darkMode = !darkMode;
+  }
+</script>
+
+<button on:click={switchMode}>{darkMode ? 'Light mode' : 'Dark mode'}</button>
+
+<style>
+
+</style>
