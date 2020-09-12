369877317867515a1df4d7a7404838f45c5165ba
Tom Claes <tom.claes@cegeka.com>
2020-09-12 23:47:22 +0200

    Card component added

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,39 @@
 <script>
 	import Invert from './components/Invert.svelte';
 	import Layout from './components/Layout.svelte';
+	import Card from './components/Card.svelte';
+
 	import { bgColor } from './store.js'
 	export let name;
+
+	const Cards = [
+		{name: 'Tom', surname: 'Claes'},
+		{name: 'Louie'},
+		{name: 'Greet'}
+	]
 
 </script>
 
 <Layout>
 	<main>
 		<h1>Hello {name}!</h1>
+		<div class="grid">
+			{#each Cards as card}
+				<Card person={card}/>
+			{/each}
+		</div>
+
 		<Invert />
 	</main>
 </Layout>
 
 
 <style>
+
+	:global(.grid) {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
 
 	main {
 		text-align: center;
---
+++ src/components/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let person;
+</script>
+
+<section>
+  <h1>{person.name} {#if person.surname}{person.surname}{/if}</h1>
+</section>
+
+<style>
+  section {
+    background: white;
+    border-radius: 5px;
+    box-shadow: 0 0 5px;
+    display: grid;
+    min-height: 250px;
+    min-width: 100px;
+    max-width: 300px;
+    margin: 1rem;
+  }
+
+  h1 { 
+    color: #333;
+  }
+</style>
